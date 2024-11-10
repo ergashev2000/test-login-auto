3934abaa355a716a73d0ae2b89b8224f90c2cf78
Islomjon <ergashevislomjon1020@gmail.com>
2024-11-10 14:43:56 +0500

    feature laddning

--- src/test.tsx
+++ src/test.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function test() {
   return (
-    <div>test</div>
+    <div>
+      lreom
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora temporibus, aliquid molestias perspiciatis cupiditate, mollitia blanditiis quibusdam facilis dolore fugit aut dolores facere laboriosam vitae doloribus nulla dolorem. Perspiciatis, cum!
+    </div>
   )
 }
